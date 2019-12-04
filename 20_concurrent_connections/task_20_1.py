8ef373eda08b1197bd8d630fd8201a9657a96a90
kladovvv <electroparovoz@gmail.com>
2019-12-04 16:50:43 +0600

    1

--- 20_concurrent_connections/task_20_1.py
+++ 20_concurrent_connections/task_20_1.py
@@ -17,3 +17,35 @@
 
 Для проверки доступности IP-адреса, используйте ping.
 '''
+import subprocess
+from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor
+
+
+def ping_ip(ip):
+    res = subprocess.run(['ping', ip], stdout=subprocess.PIPE, shell=True, encoding='cp866')
+    if res.returncode == 0 and 'TTL' in str(res):
+        return True
+    else:
+        return False
+
+
+def ping_ip_addresses(ip_list, limit=3):
+    good = []
+    bad = []
+    start_time = datetime.now()
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        result = executor.map(ping_ip, ip_list)
+        for ip, res in zip(ip_list, result):
+            if res:
+                good.append(ip)
+            else:
+                bad.append(ip)
+    print(datetime.now() - start_time)
+    return good, bad
+
+
+ip_good, ip_bad = ping_ip_addresses(['10.135.7.52', '10.135.7.54', '8.8.8.8', '1.1.1.1', '87.250.250.242', '9.9.9.9'],
+                                    limit=6)
+print('Список доступных IP: ', ip_good)
+print('Список недоступных IP: ', ip_bad)
