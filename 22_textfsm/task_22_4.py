ce51997d9106a22ac9792c39671871b6f3b9a5c9
kladovvv <electroparovoz@gmail.com>
2019-12-20 13:48:13 +0600

    1

--- 22_textfsm/task_22_4.py
+++ 22_textfsm/task_22_4.py
@@ -18,3 +18,26 @@
 
 Проверить работу функции на примере вывода команды sh ip int br и устройствах из devices.yaml.
 '''
+import textfsm
+import yaml
+import netmiko
+import os
+
+
+def send_and_parse_show_command(device_dict, command, templates_path):
+    with netmiko.ConnectHandler(**device_dict) as ssh:
+        output = ssh.send_command(command)
+    path = os.path.join(templates_path, 'sh_ip_int_br.template')
+    with open(path) as template:
+        fsm = textfsm.TextFSM(template)
+        data = fsm.ParseTextToDicts(output)
+    return data
+
+
+with open('devices.yaml') as dev:
+    devices = yaml.safe_load(dev)
+command = 'sh ip int br'
+templates = os.path.join(os.getcwd(), 'templates')
+for device in devices:
+    print(device['ip'])
+    print(send_and_parse_show_command(device, command, templates))
--- 22_textfsm/task_22_5.py
+++ 22_textfsm/task_22_5.py
@@ -13,3 +13,48 @@
 
 Теста для этого задания нет.
 '''
+import textfsm
+import yaml
+import netmiko
+import os
+from datetime import datetime
+import logging
+from concurrent.futures import ThreadPoolExecutor
+from itertools import repeat
+
+
+logging.getLogger("paramiko").setLevel(logging.WARNING)
+logging.basicConfig(
+    format='%(threadName)s: %(message)s',
+    level=logging.INFO)
+
+
+def send_and_parse_show_command(device_dict, command, templates_path):
+    logging.info('START:')
+    logging.info(datetime.now())
+    with netmiko.ConnectHandler(**device_dict) as ssh:
+        output = ssh.send_command(command)
+    with open(templates_path) as template:
+        fsm = textfsm.TextFSM(template)
+        data = fsm.ParseTextToDicts(output)
+    logging.info('STOP:')
+    logging.info(datetime.now())
+    return data
+
+
+with open('devices.yaml') as dev:
+    devices = yaml.safe_load(dev)
+command = 'sh ip int br'
+templates = os.path.join(os.getcwd(), 'templates', 'sh_ip_int_br.template')
+
+
+def send_and_parse_command_parallel(devices, command, templates):
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        result = executor.map(send_and_parse_show_command, devices, repeat(command), repeat(templates))
+        for d, o in zip(devices, result):
+            print(d['ip'], '\n', o)
+
+
+start_time = datetime.now()
+send_and_parse_command_parallel(devices, command, templates)
+print(datetime.now() - start_time)
